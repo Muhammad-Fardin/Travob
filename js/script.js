c7d7c682c21145e960c77ce6211a6e5860ba3721
Muhammad Fardin <fardinbmw5@gmail.com>
2022-05-22 21:09:47 +0530

    optimized

--- js/script.js
+++ js/script.js
@@ -1,15 +1,8 @@
 let menu = document.querySelector("#menu-btn");
 let navbar = document.querySelector(".nav-links");
 
-window.addEventListener("load", (e) => {
-  document.querySelector(".class").classList.add("fadeout");
-  document.querySelector(".class1").classList.add("fadein");
-  setTimeout(() => {
-    document.querySelector(".class").classList.add("hide");
-  }, 999);
-});
 
-menu.onclick = () => {
+menu.onClick = () => {
   menu.classList.toggle("fa-times");
   navbar.classList.toggle("active");
 };
